c52d60e1645822026fc34c99edd4bc643cfcef24
Alexander Bruy <alexander.bruy@gmail.com>
2017-07-27 11:59:15 +0300

    update license headers

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@
     __init__.py
     ---------------------
     Date                 : May 2014
-    Copyright            : (C) 2014 by Alexander Bruy
+    Copyright            : (C) 2014-2017 by Alexander Bruy
     Email                : alexander dot bruy at gmail dot com
 ***************************************************************************
 *                                                                         *
@@ -19,7 +19,7 @@
 
 __author__ = 'Alexander Bruy'
 __date__ = 'May 2014'
-__copyright__ = '(C) 2014, Alexander Bruy'
+__copyright__ = '(C) 2014-2017, Alexander Bruy'
 
 # This will get replaced with a git SHA1 when you do a git archive
 
