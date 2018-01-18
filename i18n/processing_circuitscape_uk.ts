f563a3ff19d50e927f848b5a3fc1f5746cb7a50e
Alexander Bruy <alexander.bruy@gmail.com>
2018-01-18 13:25:58 +0200

    update README
    (cherry picked from commit d96a6cd309d75f17631e40fc20693f1e80f3af3c)

--- i18n/processing_circuitscape_uk.ts
+++ i18n/processing_circuitscape_uk.ts
@@ -3,102 +3,102 @@
 <context>
     <name>Advanced</name>
     <message>
-        <location filename="../Advanced.py" line="66"/>
+        <location filename="../advanced.py" line="66"/>
         <source>Advanced</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="69"/>
+        <location filename="../advanced.py" line="69"/>
         <source>Circuitscape</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="78"/>
+        <location filename="../advanced.py" line="78"/>
         <source>Keep both when possible</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="78"/>
+        <location filename="../advanced.py" line="78"/>
         <source>Remove source</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="78"/>
+        <location filename="../advanced.py" line="78"/>
         <source>Remove ground</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="78"/>
+        <location filename="../advanced.py" line="78"/>
         <source>Remove both</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="83"/>
+        <location filename="../advanced.py" line="83"/>
         <source>Raster resistance map</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="85"/>
+        <location filename="../advanced.py" line="85"/>
         <source>Data represent conductances instead of resistances</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="88"/>
+        <location filename="../advanced.py" line="88"/>
         <source>Current source</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="90"/>
+        <location filename="../advanced.py" line="90"/>
         <source>Ground point</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="92"/>
+        <location filename="../advanced.py" line="92"/>
         <source>Data represent conductances instead of resistances to ground</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="95"/>
+        <location filename="../advanced.py" line="95"/>
         <source>When a source and ground are at the same node</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="100"/>
+        <location filename="../advanced.py" line="100"/>
         <source>Use unit currents (i = 1) for all current sources</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="103"/>
+        <location filename="../advanced.py" line="103"/>
         <source>Use direct connections to ground (R = 0) for all ground points</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="106"/>
+        <location filename="../advanced.py" line="106"/>
         <source>Create current map</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="109"/>
+        <location filename="../advanced.py" line="109"/>
         <source>Create voltage map</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="112"/>
+        <location filename="../advanced.py" line="112"/>
         <source>Mask raster</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="115"/>
+        <location filename="../advanced.py" line="115"/>
         <source>Short-circuit region</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="118"/>
+        <location filename="../advanced.py" line="118"/>
         <source>Output basename</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Advanced.py" line="122"/>
+        <location filename="../advanced.py" line="122"/>
         <source>Output directory</source>
         <translation type="unfinished"></translation>
     </message>
@@ -106,57 +106,57 @@
 <context>
     <name>CircuitscapeProvider</name>
     <message>
-        <location filename="../CircuitscapeProvider.py" line="64"/>
+        <location filename="../circuitscapeProvider.py" line="64"/>
         <source>Activate</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CircuitscapeProvider.py" line="69"/>
+        <location filename="../circuitscapeProvider.py" line="69"/>
         <source>Circuitscape directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CircuitscapeProvider.py" line="75"/>
+        <location filename="../circuitscapeProvider.py" line="75"/>
         <source>Log commands output</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CircuitscapeProvider.py" line="80"/>
+        <location filename="../circuitscapeProvider.py" line="80"/>
         <source>Connect raster cells to 4 neighbors instead of 8</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CircuitscapeProvider.py" line="84"/>
+        <location filename="../circuitscapeProvider.py" line="84"/>
         <source>Use average conductance instead of resistance for connections between cells</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CircuitscapeProvider.py" line="88"/>
+        <location filename="../circuitscapeProvider.py" line="88"/>
         <source>Preemptively release memory when possible</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CircuitscapeProvider.py" line="92"/>
+        <location filename="../circuitscapeProvider.py" line="92"/>
         <source>Write maximum of current maps</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CircuitscapeProvider.py" line="96"/>
+        <location filename="../circuitscapeProvider.py" line="96"/>
         <source>Write cumulative &amp; maximum current maps only</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CircuitscapeProvider.py" line="100"/>
+        <location filename="../circuitscapeProvider.py" line="100"/>
         <source>Set focal nodes currents to zero</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CircuitscapeProvider.py" line="104"/>
+        <location filename="../circuitscapeProvider.py" line="104"/>
         <source>Log-transform current maps</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CircuitscapeProvider.py" line="108"/>
+        <location filename="../circuitscapeProvider.py" line="108"/>
         <source>Compress output grids</source>
         <translation type="unfinished"></translation>
     </message>
@@ -164,77 +164,77 @@
 <context>
     <name>OneToAll</name>
     <message>
-        <location filename="../OneToAll.py" line="63"/>
+        <location filename="../oneToAll.py" line="63"/>
         <source>One-to-all / All-to-one</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../OneToAll.py" line="66"/>
+        <location filename="../oneToAll.py" line="66"/>
         <source>Circuitscape</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../OneToAll.py" line="75"/>
+        <location filename="../oneToAll.py" line="75"/>
         <source>One to all</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../OneToAll.py" line="75"/>
+        <location filename="../oneToAll.py" line="75"/>
         <source>All to one</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../OneToAll.py" line="78"/>
+        <location filename="../oneToAll.py" line="78"/>
         <source>Modelling mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../OneToAll.py" line="83"/>
+        <location filename="../oneToAll.py" line="83"/>
         <source>Resistance map</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../OneToAll.py" line="85"/>
+        <location filename="../oneToAll.py" line="85"/>
         <source>Data represent conductances instead of resistances</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../OneToAll.py" line="88"/>
+        <location filename="../oneToAll.py" line="88"/>
         <source>Focal node location</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../OneToAll.py" line="90"/>
+        <location filename="../oneToAll.py" line="90"/>
         <source>Create current map</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../OneToAll.py" line="93"/>
+        <location filename="../oneToAll.py" line="93"/>
         <source>Create voltage map</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../OneToAll.py" line="96"/>
+        <location filename="../oneToAll.py" line="96"/>
         <source>Mask raster</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../OneToAll.py" line="99"/>
+        <location filename="../oneToAll.py" line="99"/>
         <source>Short-circuit region</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../OneToAll.py" line="102"/>
+        <location filename="../oneToAll.py" line="102"/>
         <source>Source strength</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../OneToAll.py" line="105"/>
+        <location filename="../oneToAll.py" line="105"/>
         <source>Output basename</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../OneToAll.py" line="109"/>
+        <location filename="../oneToAll.py" line="109"/>
         <source>Output directory</source>
         <translation type="unfinished"></translation>
     </message>
@@ -242,67 +242,67 @@
 <context>
     <name>Pairwise</name>
     <message>
-        <location filename="../Pairwise.py" line="62"/>
+        <location filename="../pairwise.py" line="62"/>
         <source>Pairwise</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Pairwise.py" line="65"/>
+        <location filename="../pairwise.py" line="65"/>
         <source>Circuitscape</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Pairwise.py" line="74"/>
+        <location filename="../pairwise.py" line="74"/>
         <source>Resistance map</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Pairwise.py" line="76"/>
+        <location filename="../pairwise.py" line="76"/>
         <source>Data represent conductances instead of resistances</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Pairwise.py" line="79"/>
+        <location filename="../pairwise.py" line="79"/>
         <source>Focal node location</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Pairwise.py" line="81"/>
+        <location filename="../pairwise.py" line="81"/>
         <source>Create current map</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Pairwise.py" line="84"/>
+        <location filename="../pairwise.py" line="84"/>
         <source>Create voltage map</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Pairwise.py" line="87"/>
+        <location filename="../pairwise.py" line="87"/>
         <source>Mask raster</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Pairwise.py" line="90"/>
+        <location filename="../pairwise.py" line="90"/>
         <source>Short-circuit region</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Pairwise.py" line="93"/>
+        <location filename="../pairwise.py" line="93"/>
         <source>Focal node pairs to exclude/include</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Pairwise.py" line="96"/>
+        <location filename="../pairwise.py" line="96"/>
         <source>Run in low memory mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Pairwise.py" line="99"/>
+        <location filename="../pairwise.py" line="99"/>
         <source>Output basename</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Pairwise.py" line="103"/>
+        <location filename="../pairwise.py" line="103"/>
         <source>Output directory</source>
         <translation type="unfinished"></translation>
     </message>
